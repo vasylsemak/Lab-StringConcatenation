1d408bb1d3b77a2ac4643b6f58428655d61aa89f
Vasyl Semak <vasylsmk1401@gmail.com>
2021-06-17 12:49:25 -0400

    practice using a common function of Strings and that is string concatenation

---
+++ src/StringConcatenation.java
@@ -0,0 +1,24 @@
+
+public class StringConcatenation {
+
+	public static void main(String[] args) {
+		String begining = "Hello";
+		String end = " world!";
+		String combo = begining + end;
+		System.out.println(combo);
+		
+		long l = 203L;
+		System.out.println("The value of l: " + l);
+		
+		boolean b = true;
+		System.out.println("boolean " + b + " is true");
+		
+		int x = 2;
+		int y = 3;
+		System.out.println("Concatinated sum of x and y is " + x + y);
+		System.out.println("Added sum of x and y is " + (x + y));
+		System.out.println(x + y + " is the sum of x and y");
+
+	}
+
+}
